86510e45a2c4e5a90210080f4fd3ebd0b771c729
Rubilmax <rmilon@gmail.com>
2023-07-04 11:32:17 +0200

    minimal case

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,16 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IContract} from "@import-bug/interfaces/IContract.sol";
+import {IContract} from "@forge-import-bug-submodule/interfaces/IContract.sol";
+
+import {Type1, Type2} from "@forge-import-bug-submodule/libraries/Types.sol";
 
 contract Contract {
     IContract target;
 
-    function test1(Type1 calldata type1) {
+    function test1(Type1 calldata type1) external {
         target.test1(type1);
     }
 
-    function test2(Type2 type2) {
+    function test2(Type2 type2) external {
         target.test2(type2);
     }
 }
